7521d40a8087f6ec674dd0bf41eb4ccba6403d9e
gaston-quispe <gastonquispe@gmail.com>
2017-09-23 22:54:13 -0300

    Se utiliza getch desde palidrome

--- main.c
+++ main.c
@@ -6,24 +6,26 @@
 
 #include <stdio.h>
 
-int getch(int fd_entrada);
-
-int putch(int fd_salida, int caracter);
+int getch(int ifd);
+int putch(int ofd, int caracter);
+int palindrome(int ifd, int ibytes);
 
 int main(void)
 {
 	// int n;
-	int v0;
+	// int v0;
 	FILE* archivo_entrada;
 	archivo_entrada = fopen("entrada","r");
 
 	FILE* archivo_salida;
 	archivo_salida = fopen("salida","w");
 
-	while ((v0 = getch(fileno(archivo_entrada))) != 256)
-		printf("%c\n", v0);
+	palindrome(fileno(archivo_entrada), 16);
 
-	if (v0 == 256) printf("FIN DE ARCHIVO\n");
+	// while ((v0 = getch(fileno(archivo_entrada))) != 256)
+	// 	printf("%c\n", v0);
+	//
+	// if (v0 == 256) printf("FIN DE ARCHIVO\n");
 
 	//putch(fileno(archivo_salida) ,'a');
 
